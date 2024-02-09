c25f1145f5a6e6fde0e42aabb24677ca57a90767
SUVIL KAUSHIK <suvilk@udel.edu>
2024-02-09 12:57:17 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Suvil Kaushik aka The Chosen One UD CISC275 with React Hooks and
-                TypeScript
+                Suvil Kaushik UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
