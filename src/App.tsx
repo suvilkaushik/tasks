fb520a345d55cba8a82fbfaa6fbd3c7081f7d29a
SUVIL KAUSHIK <suvilk@udel.edu>
2024-02-09 15:01:02 -0500

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Suvil Kaushik 1 UD CISC275 with React Hooks and TypeScript
+                Suvil Kaushik UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Hello World!
             </p>
         </div>
     );
